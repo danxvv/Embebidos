c763a7fb2f2709a94a3d7d5536926456dd5ca0a0
danxvv <danxvv@gmail.com>
2019-04-29 23:00:34 -0500

    deleted:    zynqtimer/zynqtimer.cache/ip/2018.3/03579added4b43bb/design_1_rst_ps7_0_100M_0.dcp

--- zynqtimer/zynqtimer.sdk/timersbsp/ps7_cortexa9_0/libsrc/openamp_v1_5/build_openamp/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/daniel/Documents/VHDL/zynqtimer/zynqtimer.sdk/timersbsp/ps7_cortexa9_0/libsrc/openamp_v1_5/src/open-amp
-# Build directory: /home/daniel/Documents/VHDL/zynqtimer/zynqtimer.sdk/timersbsp/ps7_cortexa9_0/libsrc/openamp_v1_5/build_openamp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(lib)
